8cc9e16b86e6a12711a7c468fcbec276b6358f0a
cauehenrique <caue313@gmail.com>
2021-10-13 15:03:43 -0300

    first commit

---
+++ components/Starter.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen flex justify-center items-center flex-col">
+    <h1 class="text-4xl font-bold mb-3">Nuxt 3 + TailwindCSS ⛰️🌬️</h1>
+    <h2 class="text-2xl text-gray-500">
+      Delete <code class="text-blue-500">/components/Starter.vue</code> to get
+      started!
+    </h2>
+  </div>
+</template>
