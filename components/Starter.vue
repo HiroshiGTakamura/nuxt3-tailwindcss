9068bd8b2efb91a5ef9e16cfb25981638ba4f7f6
Cauê Henrique <caue313@gmail.com>
2021-10-28 11:01:29 -0300

    update: headwind stuff

--- components/Starter.vue
+++ components/Starter.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="min-h-screen flex justify-center items-center flex-col">
-    <h1 class="text-4xl font-bold mb-3">Nuxt 3 + TailwindCSS ⛰️🌬️</h1>
+  <div class="flex flex-col items-center justify-center min-h-screen">
+    <h1 class="mb-3 text-4xl font-bold">Nuxt 3 + TailwindCSS ⛰️🌬️</h1>
     <h2 class="text-2xl text-gray-500">
-      Delete <code class="text-blue-500">/components/Starter.vue</code> to get
-      started!
+      Delete
+      <code class="text-blue-500">/components/Starter.vue</code> to get started!
     </h2>
   </div>
 </template>
