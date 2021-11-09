43f1b6e3632b353b9078e68605949138432d77fc
HiroshiGTakamura <pedrohiroshigodenytakamura@gmail.com>
2021-11-09 19:03:59 -0300

    mudança final verdadeira

--- pages/sobrenos.vue
+++ pages/sobrenos.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="px-5 py-10 md:px-44">
 		<h1 class="mb-2 text-3xl font-bold">Sobre Nós</h1>
-		<p class="text-lg">
+		<p class="text-lg text-justify">
 			A Oficina Online trabalha para facilitar a vida dos clientes de carro no processo de serviços de mecânica automotiva. Acreditamos que com mais acesso a informação os clientes podem ter mais poder de escolha na hora de encontrar uma oficina e ter seu carro consertado. E com este site podemos dar todas as informações possíveis sobre nossa loja para auxiliá-lo a fazer a escolha certa. Nosso objetivo é transformar a experiência do cliente em algo menos estressante, conectando-o conosco e
 			prestando um serviço de qualidade a preços justos.
 			<br /><br />
@@ -18,7 +18,7 @@
 			• Ter profundo conhecimento técnico;
 		</p>
 		<h1 class="mt-4 mb-2 text-3xl font-bold">Nossa História</h1>
-		<p class="text-lg">
+		<p class="text-lg text-justify">
 			Movidos pela vontade de empreender e a dificuldade em contratar serviços mecânicos, tivemos a ideia de fundar a nossa própria oficina mecânica no interior do estado de São Paulo no início de 2016. <br />
 			Por cinco anos nós trabalhamos arduamente para sempre trazer o melhor serviço para você e seu carro, porém, tivermos que fechar as portas temporáriamente, e enquanto isso, estávamos debatendo soluções para este problema, quando um dia, tivemos uma idéia.<br />
 			Decidimos fazer um site para abranger um publico maior para a nossa oficina, o que nos resultou neste site.
